7a205ffc3ba79ea3c6436b2dc2df595311d415ac
sheep <pierre.coric.lg@gmail.com>
2023-12-09 08:58:43 -0500

    defining breakpoints

---
+++ src/breakpoints.jsx
@@ -0,0 +1,11 @@
+const breakpoints = {
+    sm : 640,
+    md : 769,
+    lg : 1024,
+    xl : 1280,
+    xxl : 1536
+};
+
+export default breakpoints;
+
+ 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,33 @@
 import React, { useState, useEffect, useRef } from 'react';
+import breakpoints from '../breakpoints';
 
 export const Card = (props) => {
+  //to hold the dimensions of the cards
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
+  //to hold the ref to the card
   const cardDivRef = useRef(null);
 
+  //useEffect hook:
   useEffect(() => {
+    //creating a function to change the dimensions of the components.
     const updateDimensions = () => {
       const divWidth = cardDivRef.current.offsetWidth;
+      //here add conditions according to the size of the window
+      console.log(breakpoints.lg);
       const divHeight = (3 / 2) * divWidth;
       setWidth(divWidth);
       setHeight(divHeight);
     };
-
+    //calling the function once when the component mounts
     updateDimensions();
-
+    //calling the function whenever the dimensions of the window change
     window.addEventListener('resize', updateDimensions);
-
+    //cleanup function
     return () => {
       window.removeEventListener('resize', updateDimensions);
     };
-  }, []);
+  }, []);//empty array of dependencies
 
   return (
     <div id={`cardDiv${props.id}`} className="col-span-1 flex items-center justify-center" style={{ height: `${height}px` }} ref={cardDivRef}>
