3be20fc32cc75da2f9062c1cc0c93918199908f4
sheep <pierre.coric.lg@gmail.com>
2023-12-10 18:07:45 -0500

    clickable cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,10 +12,13 @@ export const Card = (props) => {
   useEffect(() => {
     //creating a function to change the dimensions of the components.
     const updateDimensions = () => {
+      cardsReset();
       const divWidth = cardDivRef.current.offsetWidth;
       //here add conditions according to the size of the window
-      console.log(breakpoints.lg);
-      const divHeight = (3 / 2) * divWidth;
+      let divHeight = (1) * divWidth;
+      if (window.innerWidth > breakpoints.md) {
+        divHeight = (3 / 2) * divWidth;
+      }
       setWidth(divWidth);
       setHeight(divHeight);
     };
@@ -29,14 +32,75 @@ export const Card = (props) => {
     };
   }, []);//empty array of dependencies
 
+  const cardsReset = () => {
+    //get all the other cards
+    const allCards = document.querySelectorAll('.card');
+    // Loop through all cards and reset them
+    allCards.forEach(card => {
+      card.classList.remove('col-span-2', 'hidden');
+      card.classList.add('col-span-1');
+    });
+  }
+
+  const clickCard = (id, image) => {
+    //reset the cards
+    cardsReset();
+
+    //get the clicked card
+    const clickedDiv = document.getElementById(`cardDiv${id}`);
+    //creat the div to hide
+    let divToHide = (null)    
+
+    //initiate how many cols we have here
+    let cols = 4;
+    let extraCol = 2;
+    //if the window is smaller than lg
+    if (window.innerWidth < breakpoints.lg) {
+      cols = 3;
+      extraCol = 1;
+    }
+    //check which div to hide
+    if(id%cols < cols - extraCol) {
+      divToHide = document.getElementById(`cardDiv${id+1}`);
+    }
+    else {
+      divToHide = document.getElementById(`cardDiv${id-1}`);
+    }
+
+    //if the window is bigger than medium
+    if (window.innerWidth > breakpoints.md) {
+      //if the card is open, closes it
+      if (clickedDiv.classList.contains('open')) {
+        clickedDiv.classList.remove('col-span-2', 'open');
+        clickedDiv.classList.add('col-span-1');
+      }
+      //otherwise, open it. Starts by hidding the neighbor
+      else {
+        if (divToHide != null) {
+          divToHide.classList.add('hidden');
+        }
+        clickedDiv.classList.remove('col-span-1');
+        clickedDiv.classList.add('col-span-2', 'open');
+      }
+    }
+    //if the window is smaller than medium
+    else {
+      if(clickedDiv.classList.contains('open')) {
+        clickedDiv.classList.remove('open');
+      }
+      else {
+        clickedDiv.classList.add('open');
+      }
+    }
+  }
+
   return (
-    <div id={`cardDiv${props.id}`} className="col-span-1 flex items-center justify-center" style={{ height: `${height}px` }} ref={cardDivRef}>
-      <div className="rounded-xl bg-cover bg-center hover:cursor-pointer w-[100%] h-[100%]" style={{ backgroundImage: `url('/images/${props.image}.jpg')` }}>
-        {/* Your content goes here
-            <h1>{props.title}</h1>
-            <p>{props.description}</p>
-            <a href={`${props.link}`}>{props.link}</a>
-            <p>{props.github}</p>*/}
+    <div id={`cardDiv${props.id}`} className="card col-span-1 flex items-center justify-center hover:cursor-pointer transition-all" style={{ height: `${height}px` }} ref={cardDivRef} onClick={() => clickCard(props.id, props.image)}>
+      <div id={`picture${props.id}`}className="rounded-xl bg-cover bg-center w-[100%] h-[100%] transition-all" style={{ backgroundImage: `url('/images/${props.image}.jpg')` }}>
+        <h1>{props.title}</h1>
+        <p>{props.description}</p>
+        <a href={`${props.link}`}>{props.link}</a>
+        <p>{props.github}</p>
       </div>
     </div>
   );
